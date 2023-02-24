fe6fa3a81b0f35b652d3c0922a4d8d8e751edf1c
EZinkov <evgeniyzinkov98@gmail.com>
2023-02-24 20:18:29 +0100

    Add intro component

---
+++ src/components/Intro.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Intro({ displaySettings }) {
+  return (
+    <div className="container-intro">
+      <h1>Quizzical</h1>
+      <p className="description">Test your knowledge, if you dare!</p>
+      <button
+        type="button"
+        className="start-btn"
+        onClick={() => displaySettings()}
+      >
+        Begin
+      </button>
+    </div>
+  )
+}
